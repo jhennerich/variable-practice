d0b3ce8f839b590e7923b47de482e51c1f4d41b7
John Hennerich <john.hennerich@gmail.com>
2021-11-04 20:39:46 -0600

    Add boolean vars

--- model1-1.rb
+++ model1-1.rb
@@ -1,12 +1,19 @@
 ###
-#
+# model1-1.rb
 ###
 
+# String vars
 pizza = "mushroom"
 name = "John"
 address = "123 Anywhere St"
 
+# Float and Intergers
 tax = 0.07
 cost_of_topping = 1.50
 order_number = 1
+
+#Boolean vars
+new_customer = true
+repeat_order = false
+
 
